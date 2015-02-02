d616bf6f74da09b7abef8d7ef57c92020599ba3a
Scott Chamberlain <myrmecocystus@gmail.com>
2015-02-02 08:31:22 -0800

    added all files, removed rcurl from deps

---
+++ R/branching-package.r
@@ -0,0 +1,14 @@
+#' traits
+#'
+#' @name traits-package
+#' @aliases traits
+#' @docType package
+#' @title Species trait data from around the web
+#' @keywords package
+NULL
+
+#' PLANTATT plant traits dataset
+#' @name plantatt
+#' @docType data
+#' @keywords data
+NULL
